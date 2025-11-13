817c97f447d12c9aad033d325ea596f5a2ad9b6f
Andrew Zhou <andrewzhou@berkeley.edu>
2025-11-12 21:05:39 -0800

    Configure basePath for /wave-poser deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').Config} */
 const nextConfig = {
+  basePath: '/wave-poser',
   // Remove webpack config for now to work with Turbopack
   // TODO: Add back webpack config if needed for TensorFlow.js
 }
